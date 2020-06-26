57cbe7e1d9484b80c61983c72b2652ff6f78cace
IanSmits <56724446+IanSmits@users.noreply.github.com>
2020-06-26 12:02:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ from words import word_list
 def get_word():
   word = random.choice(word_list)
   return word.upper()
+
+def play(word):
+  word_completion = "-" * len(word)
+  guessed = False
+  guessed_letters = []
+  tries = 5
