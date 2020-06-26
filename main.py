380bbb4f9c5d02d53961584e62a2b0aae71ca684
IanSmits <56724446+IanSmits@users.noreply.github.com>
2020-06-26 12:09:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -33,3 +33,8 @@ def play(word):
         guessed_letters.append(guess)
         word_as_list = list(word_completion)
         indices = [i for i, letter in enumerate(word) if letter == guess]
+        for index in indices:
+          word_as_list[index] = guess
+        word_completion = "".join(word_as_list)
+        if "-" not in word_completion:
+          guessed = True
